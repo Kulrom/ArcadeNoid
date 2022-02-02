7f127e45131e839e9f8196bde7ed62af3b36444d
Роман Кулаков <kulromgame@gmail.com>
2022-02-02 06:38:44 +0300

    транспонирование аккордов

--- code/test.py
+++ code/test.py
@@ -1,4 +1,16 @@
-my_family = ['Рома', 'Ира', 'Лера']
+def transponate(chord: str, distance: int) -> str:
+    chord_list = ['A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb','E', 'F', 'F#', 'G', 'G#' ]
+    i = 0
+    while chord_list[i] != chord:
+        i += 1
 
-for i in my_family:
-    print(i.upper())
+    i = (i + distance) % 12
+    return chord_list[i]
+
+def main():
+    print("транспонатор аккордов")
+    print(transponate('B', -3))
+
+
+if __name__ == '__main__':
+    main()
